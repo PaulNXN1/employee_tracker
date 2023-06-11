666c503e85688e1a8e37f799405a9592413d0e2a
Paul Nixon <paulnxn1@gmail.com>
2023-06-10 22:00:09 -0400

    updated schema

---
+++ database/schema.sql
@@ -0,0 +1,45 @@
+DROP DATABASE IF EXIST employee_tracker_db;
+CREATE DATABASE employee_tracker_db;  
+
+USE employee_tracker_db; 
+
+
+
+CREATE TABLE department (
+
+id INT PRIMARY KEY AUTO_INCREMENT,
+name VARCHAR(30) NOT NULL
+
+);
+
+
+CREATE TABLE role (
+
+id INT PRIMARY KEY AUTO_INCREMENT,
+title VARCHAR(30),
+salary DECIMAL, 
+department_id INT NOT NULL, 
+FOREIGN KEY (department_id) 
+REFERENCES department(id)
+ON DELETE CASCADE
+
+);
+
+
+
+CREATE TABLE employee (
+
+id INT PRIMARY KEY AUTO_INCREMENT,
+first_name VARCHAR(30),
+last_name VARCHAR(30),
+role_id INT NOT NULL,
+manager_id INT
+FOREIGN KEY (role_id)
+REFERENCES role(id)
+ON DELETE CASCADE,
+
+FOREIGN KEY (manager_id)
+REFERENCES employee(id)
+ON DELETE SET NULL
+
+);
