39afa1cc94ad4293d5e2bdb4df4b9e83c7dcad6b
Paul Nixon <paulnxn1@gmail.com>
2023-06-12 16:01:55 -0400

    Updated ReadME file

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # employee_tracker
+
+## Description 💻
+
+UPenn Module #12 is an assignment to work on MySQL.  The student was tasked to create interfaces that allow the user to interact with databases, called **content management systems (CMS).**
+
+The goal is to create, or program a command-line application with no starter code.  Node.js, Inquirer, and MySQL were used to finalize this application.  
+
+A homepage, index.ejs was used as practice with routing for the user to see a homepage.  This was not a requirement of the assignment. 
+
+
+## Code Contributors
+
+The project was completed by each student. This was completed by Paul Nixon.
+
+
+## User Story
+
+As a user, you will be able to cycle through an employee database and create, or add new items within the database. 
+
+
+## Acceptance Criteria
+
+The project is complete when all Module #12 assignment criteria are met. 
+
+
+## Deployment
--- views/index.js
+++ views/index.js
@@ -18,6 +18,124 @@ const questions = [
   }];
 
 
+function viewDepartments() {
+
+  connection.query(
+    'SELECT * FROM department',
+    function (err, results, fields) {
+      console.table(results); // results contains rows returned by server
+
+      init();
+    }
+  );
+
+};
+
+
+function viewRoles() {
+
+  connection.query(
+    'SELECT * FROM role',
+    function (err, results, fields) {
+      console.table(results); // results contains rows returned by server
+
+      init();
+    }
+  );
+
+};
+
+
+function viewEmployees() {
+
+  connection.query(
+    'SELECT * FROM employee',
+    function (err, results, fields) {
+      console.table(results); // results contains rows returned by server
+
+      init();
+    }
+  );
+
+};
+
+
+
+function addDepartment() {
+
+
+  inquirer.prompt([{
+    type: 'input',
+    message: 'What is the name of the department?',
+    name: 'departmentName'
+  }]).then(function (answers) {
+
+    connection.query(
+      "INSERT INTO department (name) VALUES (?)", [answers.departmentName] , function (err, results, fields) {
+        console.table(results); // results contains rows returned by server
+
+        init();
+      }
+
+    )
+  })
+
+
+
+  // connection.query(
+  //   'SELECT * FROM department',
+  //   function (err, results, fields) {
+  //     console.table(results); // results contains rows returned by server
+
+  //     init();
+  //   }
+  // );
+
+};
+
+
+function addRole() {
+
+  connection.query(
+    'SELECT * FROM department',
+    function (err, results, fields) {
+      console.table(results); // results contains rows returned by server
+
+      init();
+    }
+  );
+
+};
+
+
+function addEmployee() {
+
+  connection.query(
+    'SELECT * FROM department',
+    function (err, results, fields) {
+      console.table(results); // results contains rows returned by server
+
+      init();
+    }
+  );
+
+};
+
+
+function updateRole() {
+
+  connection.query(
+    'SELECT * FROM department',
+    function (err, results, fields) {
+      console.table(results); // results contains rows returned by server
+
+      init();
+    }
+  );
+
+};
+
+
 
 // function that starts the prompt to the user using node.js
 function init() {
@@ -28,47 +146,46 @@ function init() {
 
       switch (answers.user_choice) {
         case 'View all departments':
-          console.log('\nViewing departments...');
+          console.log('\nViewing departments...\n\n');
+          viewDepartments();
           break;
 
+
         case 'View all roles':
-          console.log('\nViewing all roles...');
+          console.log('\nViewing all roles...\n\n');
+          viewRoles();
           break;
 
         case 'View all employees':
-          console.log('\nViewing all employees...');
+          console.log('\nViewing all employees...\n\n');
+          viewEmployees();
           break;
 
         case 'Add a department':
-          console.log('\nAdding a department...');
+          console.log('\nAdding a department...\n\n');
+          addDepartment();
+
           break;
 
         case 'Add a role':
-          console.log('\nAdding a role...');
+          console.log('\nAdding a role...\n\n');
           break;
 
 
         case 'Add an employee':
-          console.log('\nAdding an employee...');
+          console.log('\nAdding an employee...\n\n');
           break;
 
         case 'Update an employee role':
-          console.log('\nUpdating role...');
+          console.log('\nUpdating role...\n\n');
           break;
 
 
       };
 
-      init();
-
-      connection.query(
-        'SELECT * FROM department',
-        function (err, results, fields) {
-          console.table(results); // results contains rows returned by server
 
 
-        }
-      );
+
 
     })
 };
